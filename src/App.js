656a74dd4c9b36da0346bbb0da015a1046a96b09
flg <pluton3@icloud.com>
2021-06-12 16:46:18 +0200

    Router v1

--- src/App.js
+++ src/App.js
@@ -1,82 +1,81 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-//import dayjs from 'dayjs';
-import ExamTable from './ExamComponents'
-
+import './App.css';
 import { fakeCourses, fakeExams } from './FakeData';
 import AppTitle from './AppTitle';
-import { useState } from 'react';
-
 import { PrivacyMode, EditMode } from './createContexts';
+import { ExamTable, ExamForm } from './ExamComponents'
 
 function App() {
 
+  const [exams, setExams] = useState([...fakeExams]);
   const [privacy, setPrivacy] = useState(false);
   const [editable, setEditable] = useState(true);
 
+  const examCodes = exams.map(exam => exam.coursecode);
+  
+  const addExam = (exam) => {
+    setExams((oldExams) => [...oldExams, exam]);
+    //setExams((oldExams) => oldExams.concat(exam));
+  };
+
+  const updateExam = (exam) => {
+    setExams(oldExams => {
+      return oldExams.map(ex => {
+        if (ex.coursecode === exam.coursecode)
+          return {coursecode: exam.coursecode, score: exam.score, date: exam.date};
+        else
+          return ex;
+      });
+    });
+  }
+
+  const deleteExam = (coursecode) => {
+    setExams((exs) => exs.filter((ex) => ex.coursecode !== coursecode));
+  };
+
+  const checkExam = (course) => {
+    if (exams.filter( ex => ex.coursecode === course).length === 0)
+        return true;
+    else
+        return false;
+};
+
   return (
-    <Container className='App'>
-      <Row>
-        <AppTitle />
-        <Col align='right'>
-          <Button variant='secondary' onClick={ () => setPrivacy( p => !p )}>{privacy ? 'View' : 'Hide'}</Button>
-          <Button variant='secondary' onClick={ () => setEditable( e => !e )}>{editable ? 'Read' : 'Edit'}</Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <PrivacyMode.Provider value={privacy}>
-            <EditMode.Provider value={editable}>
-              <ExamTable exams={fakeExams} courses={fakeCourses} />
-            </EditMode.Provider>
-          </PrivacyMode.Provider>
-        </Col>
-      </Row>
-    </Container>
+    <Router>
+      <Container className='App'>
+        <Row>
+          <AppTitle />
+          <Col align='right'>
+            <Button variant='secondary' onClick={() => setPrivacy(p => !p)}>{privacy ? 'View' : 'Hide'}</Button>
+            <Button variant='secondary' onClick={() => setEditable(e => !e)}>{editable ? 'Read' : 'Edit'}</Button>
+          </Col>
+        </Row>
+        <Switch>
+        <Route path="/add" render={() => 
+         <ExamForm courses={fakeCourses.filter(course => !examCodes.includes(course.coursecode))} 
+                  addOrUpdateExam={addExam} 
+                  checkExam={checkExam} /> }/>
+        <Route path="/update" render={() => 
+          <ExamForm courses={fakeCourses} 
+                    addOrUpdateExam={updateExam} checkExam={checkExam}/> }/>
+        <Route path='/' render={ () => 
+           <Row>
+             <Col>
+               <PrivacyMode.Provider value={privacy}>
+                 <EditMode.Provider value={editable}>
+                   <ExamTable exams={fakeExams} courses={fakeCourses} deleteExam={deleteExam}/>
+                  </EditMode.Provider>
+                </PrivacyMode.Provider>
+              </Col>
+            </Row>} />
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
 export default App;
-
-/*
-
-<Table striped bordered hover>
-<thead>
-  <tr>
-    <th>Exam</th>
-    <th>Score</th>
-    <th>Date</th>
-    <th>Actions</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td>Information systems security</td>
-    <td>28</td>
-    <td>01/03/2021</td>
-    <td>{iconEdit} {iconDelete}</td>
-  </tr>
-  <tr>
-    <td>Data Science and Database Technology</td>
-    <td>29</td>
-    <td>03/06/2021</td>
-    <td>{iconEdit} {iconDelete}</td>
-  </tr>
-  <tr>
-    <td>Software Engineering</td>
-    <td>18</td>
-    <td>24/05/2021</td>
-    <td>{iconEdit} {iconDelete}</td>
-  </tr>
-  <tr>
-    <td>Web Applications I</td>
-    <td>24</td>
-    <td>21/06/2021</td>
-    <td>{iconEdit} {iconDelete}</td>
-  </tr>
-</tbody>
-</Table>
-
-*/
--- src/ExamComponents.js
+++ src/ExamComponents.js
@@ -1,29 +1,12 @@
 import { Form, Table, Button, Alert } from "react-bootstrap";
 import { useState, useContext } from "react";
+import { Link, Redirect, useLocation } from 'react-router-dom';
 import { iconDelete, iconEdit } from './icons';
 import dayjs from 'dayjs';
 
 import { PrivacyMode, EditMode } from './createContexts';
 
-function ExamTable(props) { 
-    const [exams, setExams] = useState([...props.exams]);
-    const [showForm, setShowForm] = useState(false);
-
-    const deleteExams = (coursecode) => {
-        setExams((exs) => exs.filter((ex) => ex.coursecode !== coursecode));
-    };
-
-    const addExam = (exam) => {
-        setExams((oldExams) => [...oldExams, exam]);
-        //setExams((oldExams) => oldExams.concat(exam));
-    };
-
-    const checkExam = (course) => {
-        if (exams.filter( ex => ex.coursecode === course).length === 0)
-            return true;
-        else
-            return false;
-    };
+function ExamTable(props) {
 
     return (
         <>
@@ -38,20 +21,16 @@ function ExamTable(props) {
                 </thead>
                 <tbody>
                     {
-                        exams.map((ex) =>
+                        props.exams.map((ex) =>
                             <ExamRow exam={ex}
                                 examName={props.courses.filter((c) => c.coursecode === ex.coursecode)[0].name}
-                                deleteExam={deleteExams}
+                                deleteExam={props.deleteExam}
                                 key={ex.coursecode}>
                             </ExamRow>
                         )}
                 </tbody>
             </Table>
-            {showForm ?
-                <ExamForm checkExam={ (course) => checkExam(course) } courses={props.courses}
-                    addExam={(exam) => { addExam(exam); setShowForm(false); }}
-                    cancel={() => setShowForm(false)} /> :
-                <Button variant='success' onClick={() => setShowForm(true)}>Add</Button>}
+            <Link to='/add'><Button variant='success'>Add</Button></Link>
         </>
     );
 };
@@ -82,98 +61,75 @@ function ExamRowData(props) {
 function RowControl(props) {
     return (
         <td>
-            {iconEdit} / <span onClick={() => props.deleteExam(props.exam.coursecode)}>{iconDelete}</span>
+            <Link to={{ pathname: '/update', state: { exam: props.exam, examDate: props.exam.date.format('YYYY-MM-DD') }}}>
+                {iconEdit}</Link> / <span onClick={() => { props.deleteExam(props.exam.coursecode) }}>{iconDelete}</span>
         </td>
     );
 };
 
 function ExamForm(props) {
+    const location = useLocation();
     const [course, setCourse] = useState('');
     const [score, setScore] = useState(18);
     const [date, setDate] = useState(dayjs());
     const [error, setError] = useState(''); //false
- //   const [validated, setValidated] = useState(false);
-    
-    const handleSubmit = (event) => {       
+    const [submitted, setSubmitted] = useState(false);
+    //   const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
         event.preventDefault();
+
         //FORM VALIDATION
-        if ( course !== '' && props.checkExam(course) && score<=31 && score>=18){
-        const exam = { coursecode: course, score: score, date: date };
-        props.addExam(exam);
+        if (course !== '' && props.checkExam(course) && score <= 31 && score >= 18) {
+            const exam = { coursecode: course, score: score, date: dayjs(date) };
+            props.addOrUpdateExam(exam);
+            setSubmitted(true);
+            // setValidated(true);
+        }
+        else if (course === '') { setError('Scegliere un corso dalla lista.'); }
+        else if (!props.checkExam(course)) { setError(`Il corso ${course} è già presente.`); }
+        else if (score >= 31 || score <= 18) { setError(`Voto ${score} non valido. Inserire un voto compreso tra 18 e 31.`); }
         // setValidated(true);
-        } 
-        else if (course === '') {setError('Scegliere un corso dalla lista.');}
-        else if (!props.checkExam(course)) {setError(`Il corso ${course} è già presente.`);}
-        else if (score>=31 || score<=18) {setError(`Voto ${score} non valido. Inserire un voto compreso tra 18 e 31.`);}
-       // setValidated(true);
-   };
+    };
 
-  // noValidate validated={validated} 
+    // noValidate validated={validated} 
 
-    return (
+    return (submitted ? <Redirect to="/" /> :
         <>
-        {error ? <Alert variant="danger" onClose={() => setError(false)} dismissible>{error}</Alert> : false }
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId='selectedCourse'>
-                <Form.Label>Course</Form.Label>
-                <Form.Control as='select' value={course} onChange={(ev) => setCourse(ev.target.value)}
-                 isValid={ course !== '' && props.checkExam(course)} 
-                 isInvalid={ !props.checkExam(course)}
-                  required>
-                    <option hidden disabled value=''>choose...</option>
-                    {props.courses.map(c => <option key={c.coursecode} value={c.coursecode}>{c.name}</option>)}
-                </Form.Control>
-                <Form.Control.Feedback type="invalid">
-              Il corso è già presente nella lista.
-            </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId='selectedScore' >
-                <Form.Label>Score</Form.Label>
-                <Form.Control type='number' min={18} max={31} value={score} onChange={(ev) => setScore(ev.target.value)}
-                isValid={score<=31 && score>=18} 
-                isInvalid={score>31 || score<18}
-                 required/>
-                <Form.Control.Feedback type="invalid">
-              Il voto deve essere compreso tra 18 e 30.
-            </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId='selectedDate'>
-                <Form.Label>Date</Form.Label>
-                <Form.Control type='date' value={date.format('YYYY-MM-DD')} onChange={ev => setDate(dayjs(ev.target.value))}/>
-            </Form.Group>
-            <Button onClick={handleSubmit}>Save</Button>
-            <Button variant='secondary' onClick={props.cancel}>Cancel</Button>
-        </Form>
+            {error ? <Alert variant="danger" onClose={() => setError(false)} dismissible>{error}</Alert> : false}
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId='selectedCourse'>
+                    <Form.Label>Course</Form.Label>
+                    <Form.Control as='select' value={course} onChange={(ev) => setCourse(ev.target.value)}
+                        isValid={course !== '' && props.checkExam(course)}
+                        isInvalid={!props.checkExam(course)} disabled={location.state}
+                        required>
+                        <option hidden disabled value=''>choose...</option>
+                        {props.courses.map(c => <option key={c.coursecode} value={c.coursecode}>{c.name}</option>)}
+                    </Form.Control>
+                    <Form.Control.Feedback type="invalid">
+                        Il corso è già presente nella lista.
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId='selectedScore' >
+                    <Form.Label>Score</Form.Label>
+                    <Form.Control type='number' min={18} max={31} value={score} onChange={(ev) => setScore(ev.target.value)}
+                        isValid={score <= 31 && score >= 18}
+                        isInvalid={score > 31 || score < 18}
+                        required />
+                    <Form.Control.Feedback type="invalid">
+                        Il voto deve essere compreso tra 18 e 30.
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId='selectedDate'>
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control type='date' value={date} onChange={ev => setDate(ev.target.value)} />
+                </Form.Group>
+                <Button onClick={handleSubmit}>Save</Button>
+                <Link to='/'><Button variant='secondary'>Cancel</Button></Link>
+            </Form>
         </>
     );
 };
 
-export default ExamTable;
-
-/* ALTERNATIVE IMPLEMENTATION, WITH "NATIVE" HTML CONTROLS (WITHOUT BOOTSTRAP) */
-/*
-function ExamForm_native(props) {
-    const [course, setCourse] = useState('');
-    const [score, setScore] = useState(30);
-    const [date, setDate] = useState(dayjs());
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const exam = { coursecode: course, score: score, date: date };
-        props.addExam(exam);
-    };
-
-    return (
-        <form>
-            <span style={{ display: 'inline-block', width: '5em' }}>Course:</span>
-            <select value={course} onChange={ev => setCourse(ev.target.value)}>
-                {props.courses.map(course => <option key={course.coursecode} value={course.coursecode}>{course.name}</option>)}
-            </select><br />
-            <span style={{ display: 'inline-block', width: '5em' }}>Score:</span>
-            <input type='number' min={18} max={31} value={score} onChange={ev => setScore(ev.target.value)} /><br />
-            <span style={{ display: 'inline-block', width: '5em' }}>Date:</span>
-            <input type='date' value={date.format('YYYY-MM-DD')} onChange={ev => setDate(dayjs(ev.target.value))} /><br />
-            <button onClick={handleSubmit}>Save</button> <button onClick={props.cancel}>Cancel</button>
-        </form >
-    )
-*/
+export { ExamTable, ExamForm };
